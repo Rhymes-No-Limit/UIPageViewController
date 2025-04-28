fcb2335e2deffe1e2fa68261073e3223d8fed368
Andrey <Rhymes.un.limit@gmail.com>
2025-04-28 16:37:58 +0300

    Add information about pages

--- UIPageViewController/PageViewController.swift
+++ UIPageViewController/PageViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 
 class PageViewController: UIPageViewController {
     
-    let presentScrinContent = [
+    let presentScreenContent = [
     "Первая страница презентации, рассказывает о том, что наше приложение из себя представляет",
     "Вторая страница презентации, рассказывает о какой-то удобной фишке приложения",
     "Третья страница презентации тоже рассказывает о чем-то очень интересном",
@@ -16,5 +16,18 @@ class PageViewController: UIPageViewController {
 
         
     }
-
+    
+    func showViewControllerAtIndex(_ index: Int) -> ContentViewController? {
+        
+        guard index >= 0 else { return nil }
+        guard index < presentScreenContent.count else { return nil }
+        guard let contentViewController = storyboard?.instantiateViewController(withIdentifier: "ContentViewController") as? ContentViewController else { return nil }
+        
+        contentViewController.presentText = presentScreenContent[index]
+        contentViewController.emoji = emojiArray[index]
+        contentViewController.currentPage = index
+        contentViewController.numberOfPages = presentScreenContent.count
+        
+        return contentViewController
+    }
 }
