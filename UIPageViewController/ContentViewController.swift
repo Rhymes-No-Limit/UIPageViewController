e8a9dc6273036814a5cf00d64ff0efa9f24289a6
Andrey <Rhymes.un.limit@gmail.com>
2025-04-28 16:17:15 +0300

    Prepare project

---
+++ UIPageViewController/ContentViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ContentViewController.swift
+//  UIPageViewController
+//
+//  Created by Андрей Терентьев on 28.04.2025.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+
+    @IBOutlet var presentTextLabel: UILabel!
+    @IBOutlet var emojiLabel: UILabel!
+    @IBOutlet var pageControl: UIPageControl!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
